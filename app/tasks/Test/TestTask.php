362cf796963d0aebc6b595e14731eb1e605d1ce9
李铭昕 <715557344@qq.com>
2017-07-22 08:46:38 +0800

    * 增加测试DEMO

--- app/tasks/Test/TestTask.php
+++ app/tasks/Test/TestTask.php
@@ -7,7 +7,10 @@ class TestTask extends \Phalcon\Cli\Task
 
     public function mainAction()
     {
-        
+        $res = di('client')->index->version();
+        print_r($res);
+        $res = di('client')->index->say();
+        print_r($res);
     }
 
 }
--- app/yar/Service/Client.php
+++ app/yar/Service/Client.php
@@ -7,14 +7,36 @@
 // | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
 // +----------------------------------------------------------------------
 namespace App\Yar\Service;
+
+use Yar_Client;
+
 class Client
 {
     protected $config = [];
+    protected $clients = [];
 
     public function __construct(array $config)
     {
         $this->config = $config;
     }
 
+    private function client($name)
+    {
+        if (isset($this->clients[$name]) && $this->clients[$name] instanceof Yar_Client) {
+            return $this->clients[$name];
+        }
 
+        $this->clients[$name] = new Yar_Client($this->config[$name]);
+        $this->clients[$name]->SetOpt(YAR_OPT_CONNECT_TIMEOUT, 1000);
+
+        return $this->clients[$name];
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->config[$name])) {
+            return $this->client($name);
+        }
+        throw new ClientException();
+    }
 }
